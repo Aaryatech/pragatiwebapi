d891bfb82e80b48cf8aa440bd30f9afc71e8d079
Aaryatech <code@atsinfosoft.com>
2018-08-11 11:12:57 +0530

    Image Controller

---
+++ src/main/java/com/ats/feedback/controller/ImageUploadController.java
@@ -0,0 +1,74 @@
+package com.ats.feedback.controller;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.ats.feedback.model.master.ErrorMessage;
+
+
+@RestController
+public class ImageUploadController {
+
+	private static String SIGN_URL = "/opt/apache-tomcat-8.5.6/webapps/happyfeast/uploads/item/";
+
+	@RequestMapping(value = { "/photoUpload" }, method = RequestMethod.POST)
+	public @ResponseBody ErrorMessage getFarmerContract(@RequestParam("file") MultipartFile uploadfile,
+			@RequestParam("imageName") String imageName, @RequestParam("type") String type) {
+
+		ErrorMessage info = new ErrorMessage();
+
+		System.out.println("File Name " + uploadfile.getOriginalFilename());
+
+		try {
+
+			saveUploadedFiles(Arrays.asList(uploadfile), imageName, type);
+
+			info.setError(false);
+			info.setMessage("File uploaded successfully");
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+			info.setError(true);
+			info.setMessage("File upload failed");
+		}
+
+		return info;
+	}
+
+	// save file
+	private void saveUploadedFiles(List<MultipartFile> files, String imageName, String type) throws IOException {
+
+		try {
+			for (MultipartFile file : files) {
+				Path path = null;
+				if (file.isEmpty()) {
+					continue;
+				}
+				if (type.equalsIgnoreCase("sign")) {
+					path = Paths.get(SIGN_URL + imageName);
+				} 
+
+				byte[] bytes = file.getBytes();
+
+				Files.write(path, bytes);
+
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+}
